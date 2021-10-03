94457312f6a4c910e35f8f95ffd1ef56e3d225b6
JDHatten <JDHatten@users.noreply.github.com>
2021-10-02 17:02:41 -0700

    Update Switch_Save_Game_Transfer.cmd

--- Switch_Save_Game_Transfer.cmd
+++ Switch_Save_Game_Transfer.cmd
@@ -1,7 +1,7 @@
 ::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
-:: Switch Emulator Save Game Transfer Script                                 ::
+:: Switch Save Game Transfer Script                                           ::
 ::     For Ryujinx and Yuzu Switch Emulators                                  ::
-::         by JDHatten                                                        ::
+::         by Grahf Azura                                                     ::
 ::             Version 1.0                                                    ::
 ::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
 ::                                                                            ::
@@ -43,23 +43,28 @@ set ryujinx_save_file_directory=%APPDATA%\Ryujinx\bis\user\save
 
 :: Yuzu Save File Directory (Get ID from the folder and not the app as it's reveresed)
 set yuzu_save_file_directory=%APPDATA%\yuzu\nand\user\save\0000000000000000\{your yuzu user id}
+set yuzu_save_file_directory=%APPDATA%\yuzu\nand\user\save\0000000000000000\3608B194A2FD5B18D1F3B29DD93B764F
 
 
 :: Array Of Game Save Directory Paths
 set /a i=1
+set switch_save_directories[0].title=Transfer All Game Saves [Backup Your Save Files First]
 :: Astral Chain
 set switch_save_directories[!i!].title=Astral Chain
 set switch_save_directories[!i!].ryujinx=%ryujinx_save_file_directory%\00000000000000{xx}\0
+set switch_save_directories[!i!].ryujinx=%ryujinx_save_file_directory%\0000000000000009\0
 set switch_save_directories[!i!].yuzu=%yuzu_save_file_directory%\01007300020FA000
 call :AddAboveGame
 :: Bayonetta
 set switch_save_directories[!i!].title=Bayonetta
 set switch_save_directories[!i!].ryujinx=%ryujinx_save_file_directory%\00000000000000{xx}\0
+set switch_save_directories[!i!].ryujinx=%ryujinx_save_file_directory%\000000000000000a\0
 set switch_save_directories[!i!].yuzu=%yuzu_save_file_directory%\010076F0049A2000
 call :AddAboveGame
 :: Bayonetta 2
 set switch_save_directories[!i!].title=Bayonetta 2
 set switch_save_directories[!i!].ryujinx=%ryujinx_save_file_directory%\00000000000000{xx}\0
+set switch_save_directories[!i!].ryujinx=%ryujinx_save_file_directory%\000000000000000b\0
 set switch_save_directories[!i!].yuzu=%yuzu_save_file_directory%\01007960049A0000
 call :AddAboveGame
 :: Bravely Default II
@@ -75,11 +80,13 @@ call :AddAboveGame
 :: Donkey Kong Country: Tropical Freeze
 set switch_save_directories[!i!].title=Donkey Kong Country: Tropical Freeze
 set switch_save_directories[!i!].ryujinx=%ryujinx_save_file_directory%\00000000000000{xx}\0
+set switch_save_directories[!i!].ryujinx=%ryujinx_save_file_directory%\0000000000000007\0
 set switch_save_directories[!i!].yuzu=%yuzu_save_file_directory%\0100C1F0051B6000
 call :AddAboveGame
 :: Final Fantasy Crystal Chronicles Remastered Edition
 set switch_save_directories[!i!].title=Final Fantasy Crystal Chronicles Remastered Edition
 set switch_save_directories[!i!].ryujinx=%ryujinx_save_file_directory%\00000000000000{xx}\0
+set switch_save_directories[!i!].ryujinx=%ryujinx_save_file_directory%\000000000000000c\0
 set switch_save_directories[!i!].yuzu=%yuzu_save_file_directory%\0100CE4010AAC000
 call :AddAboveGame
 :: Fire Emblem: Three Houses
@@ -90,16 +97,19 @@ call :AddAboveGame
 :: Luigi's Mansion 3
 set switch_save_directories[!i!].title=Luigi's Mansion 3
 set switch_save_directories[!i!].ryujinx=%ryujinx_save_file_directory%\00000000000000{xx}\0
+set switch_save_directories[!i!].ryujinx=%ryujinx_save_file_directory%\000000000000000d\0
 set switch_save_directories[!i!].yuzu=%yuzu_save_file_directory%\0100DCA0064A6000
 call :AddAboveGame
 :: Mario + Rabbids Kingdom Battle
 set switch_save_directories[!i!].title=Mario + Rabbids Kingdom Battle
 set switch_save_directories[!i!].ryujinx=%ryujinx_save_file_directory%\00000000000000{xx}\0
+set switch_save_directories[!i!].ryujinx=%ryujinx_save_file_directory%\000000000000000e\0
 set switch_save_directories[!i!].yuzu=%yuzu_save_file_directory%\0100E46003042000
 call :AddAboveGame
 :: Mario Kart 8 Deluxe
 set switch_save_directories[!i!].title=Mario Kart 8 Deluxe
 set switch_save_directories[!i!].ryujinx=%ryujinx_save_file_directory%\00000000000000{xx}\0
+set switch_save_directories[!i!].ryujinx=%ryujinx_save_file_directory%\000000000000000f\0
 set switch_save_directories[!i!].yuzu=%yuzu_save_file_directory%\0100152000022000
 call :AddAboveGame
 :: Pikmin 3 Deluxe
@@ -110,6 +120,7 @@ call :AddAboveGame
 :: Splatoon 2
 set switch_save_directories[!i!].title=Splatoon 2
 set switch_save_directories[!i!].ryujinx=%ryujinx_save_file_directory%\00000000000000{xx}\0
+set switch_save_directories[!i!].ryujinx=%ryujinx_save_file_directory%\0000000000000011\0
 set switch_save_directories[!i!].yuzu=%yuzu_save_file_directory%\01003BC0000A0000
 call :AddAboveGame
 :: Super Mario 3D All-Stars
@@ -120,11 +131,13 @@ call :AddAboveGame
 :: Super Mario 3D World + Bowser's Fury
 set switch_save_directories[!i!].title=Super Mario 3D World + Bowser's Fury
 set switch_save_directories[!i!].ryujinx=%ryujinx_save_file_directory%\00000000000000{xx}\0
+set switch_save_directories[!i!].ryujinx=%ryujinx_save_file_directory%\0000000000000012\0
 set switch_save_directories[!i!].yuzu=%yuzu_save_file_directory%\010028600EBDA000
 call :AddAboveGame
 :: Super Mario Odyssey
 set switch_save_directories[!i!].title=Super Mario Odyssey
 set switch_save_directories[!i!].ryujinx=%ryujinx_save_file_directory%\00000000000000{xx}\0
+set switch_save_directories[!i!].ryujinx=%ryujinx_save_file_directory%\0000000000000002\0
 set switch_save_directories[!i!].yuzu=%yuzu_save_file_directory%\0100000000010000
 call :AddAboveGame
 :: Super Mario Party
@@ -140,6 +153,7 @@ call :AddAboveGame
 :: The Legend of Zelda: Breath of the Wild
 set switch_save_directories[!i!].title=The Legend of Zelda: Breath of the Wild
 set switch_save_directories[!i!].ryujinx=%ryujinx_save_file_directory%\00000000000000{xx}\0
+set switch_save_directories[!i!].ryujinx=%ryujinx_save_file_directory%\0000000000000001\0
 set switch_save_directories[!i!].yuzu=%yuzu_save_file_directory%\01007EF00011E000
 call :AddAboveGame
 :: The Legend of Zelda: Link's Awakening
@@ -151,6 +165,7 @@ call :AddAboveGame
 :: Xenoblade Chronicles: Definitive Edition
 set switch_save_directories[!i!].title=Xenoblade Chronicles: Definitive Edition
 set switch_save_directories[!i!].ryujinx=%ryujinx_save_file_directory%\00000000000000{xx}\0
+set switch_save_directories[!i!].ryujinx=%ryujinx_save_file_directory%\0000000000000008\0
 set switch_save_directories[!i!].yuzu=%yuzu_save_file_directory%\0100FF500E34A000
 call :AddAboveGame
 :: Xenoblade Chronicles 2
@@ -182,8 +197,11 @@ call :Start
 :: Menu Loop
 :Start
     call echo ================================================================
-    for %%i in (%switch_save_directories.indexes%) do (
+    for %%i in (0 %switch_save_directories.indexes%) do (
         call echo   [!check%%i!] %%i. !switch_save_directories[%%i].title!
+        if %%i==0 (
+            call echo ================================================================
+        )
     )
     call echo ================================================================
     set /p game="--> Which game save files do you wish to transfer? Enter # "
@@ -197,15 +215,31 @@ call :Start
     set /p emu="--> From which emulator do you want to transfer to? Enter # "
     call echo/
     
-    call echo     You chose to transfer [!switch_save_directories[%game%].title!] save game files [from !transfer_from[%emu%]!].
-    set /p confirm="--> Is this correct? [y/n]"
-    call echo/
+    if "!game!"=="0" (
+        call echo     You chose to transfer [All] save game files [from !transfer_from[%emu%]!].
+        call echo     Are you absolutely sure because you can easily overwrite files you might not intend to?
+        set /p confirm="--> Have you backed up all your game saves and ready to proceed? [y/n]
+        set transfer_all=True
+    ) else (
+        call echo     You chose to transfer [!switch_save_directories[%game%].title!] save game files [from !transfer_from[%emu%]!].
+        set /p confirm="--> Is this correct? [y/n]"
+        set transfer_all=False
+        call echo/
+    )
     
     if "!confirm!"=="y" (
-        call :TransferSaveFilesFrom "!game!", "!emu!"
+        if "!transfer_all!"=="True" (
+            call :TransferAllSaveFilesFrom "!emu!"
+        ) else (
+            call :TransferSaveFilesFrom "!game!", "!emu!"
+        )
     ) else (
     if "!confirm!"=="Y" (
-        call :TransferSaveFilesFrom "!game!", "!emu!"
+        if "!transfer_all!"=="True" (
+            call :TransferAllSaveFilesFrom "!emu!"
+        ) else (
+            call :TransferSaveFilesFrom "!game!", "!emu!"
+        )
     ) else (
         call :Start
     ))
@@ -265,7 +299,15 @@ call :Start
         )
         call :CreateBackupOf "!paste_folder!"
         robocopy /E "!copy_folder!" "!paste_folder!"
+        call :TransferSaveCheck "%%i"
     )
+    call :TransferSaveCheck "0"
+    exit /b 0
+
+
+:: %1 Index
+:TransferSaveCheck
+    set check%~1=x
     exit /b 0
 
 
@@ -281,11 +323,5 @@ call :Start
 :AddAboveGame
     set "switch_save_directories.indexes=!switch_save_directories.indexes!!i! "
     set /a i=i+1
-    exit /b 0
-
-
-:: %1 Index
-:TransferSaveCheck
-    set check%~1=x
     exit /b 0
 
